fb7e396040963bbfb6f77b467abf5370dea2cf7f
sagara12 <sagara12@naver.com>
2022-04-13 23:09:49 +0900

    0220-04-13 프로젝트(개인) 커밋

---
+++ src/main/java/hello/core/CoreApplication.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//core Test
+@SpringBootApplication
+public class CoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplication.class, args);
+	}
+
+}
