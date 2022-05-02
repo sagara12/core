b5239921f0fae1a81024db3fe9d40fbe05bb5318
sagara12 <sagara12@naver.com>
2022-05-02 19:44:01 +0900

    0220-04-13 프로젝트(개인) 커밋 2022-04-14 프로젝트(개인) 기본 기능 구현 2022-04-15 제이유닛 테스트 기본 설정 2022-04-15 주문과 할인 기능 구현 2022-04-15 주문과 할인 기능 제이유닛 테스트 구현 2022-04-16 할인 정책 변경 및 제이유닛 테스트 2022-04-25 의존성 주입형식으로 변경 2022-04-25 DI 컨테이너 생성 2022-04-26 스프링 변환 2022-04-28 컨테이너에 등록된 빈 조회 2022-04-28 스프링 빈 조회 2022-04-28 동일한 타입이 둘 이상일경우 스프링 빈 조회 2022-04-30 스프링 빈 조회 - 상속관계 2022-05-01 XML 생성 2022-05-02 웹어플리케이션 과 싱글톤

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SingletonTest {
 
     @Test
@@ -23,6 +25,20 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         //memeberServoce  != memberService2
-        Assertions.assertThat(memberService).isNotEqualTo(memberService2);
+        assertThat(memberService).isNotEqualTo(memberService2);
+
     }
+
+    @Test
+    @DisplayName("스프링이 없는 순수한 DI 컨테이너")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
 }
