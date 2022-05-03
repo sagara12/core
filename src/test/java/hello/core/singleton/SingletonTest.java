f4b3e52e27c81692f829f331a060f9a14a8ae87f
sagara12 <sagara12@naver.com>
2022-05-03 14:34:27 +0900

    0220-04-13 프로젝트(개인) 커밋 2022-04-14 프로젝트(개인) 기본 기능 구현 2022-04-15 제이유닛 테스트 기본 설정 2022-04-15 주문과 할인 기능 구현 2022-04-15 주문과 할인 기능 제이유닛 테스트 구현 2022-04-16 할인 정책 변경 및 제이유닛 테스트 2022-04-25 의존성 주입형식으로 변경 2022-04-25 DI 컨테이너 생성 2022-04-26 스프링 변환 2022-04-28 컨테이너에 등록된 빈 조회 2022-04-28 스프링 빈 조회 2022-04-28 동일한 타입이 둘 이상일경우 스프링 빈 조회 2022-04-30 스프링 빈 조회 - 상속관계 2022-05-01 XML 생성 2022-05-02 웹어플리케이션 과 싱글톤 2022-05-03 싱글톤 컨테이너

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,4 +43,21 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
     }
 
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+
+        //AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        //1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memeberServoce  != memberService2
+        assertThat(memberService).isSameAs(memberService2);
+    }
 }
