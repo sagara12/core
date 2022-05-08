1a72a5c64a495de05d1618157beaf7936e64a4f2
sagara12 <sagara12@naver.com>
2022-05-08 15:17:09 +0900

    0220-04-13 프로젝트(개인) 커밋 2022-04-14 프로젝트(개인) 기본 기능 구현 2022-04-15 제이유닛 테스트 기본 설정 2022-04-15 주문과 할인 기능 구현 2022-04-15 주문과 할인 기능 제이유닛 테스트 구현 2022-04-16 할인 정책 변경 및 제이유닛 테스트 2022-04-25 의존성 주입형식으로 변경 2022-04-25 DI 컨테이너 생성 2022-04-26 스프링 변환 2022-04-28 컨테이너에 등록된 빈 조회 2022-04-28 스프링 빈 조회 2022-04-28 동일한 타입이 둘 이상일경우 스프링 빈 조회 2022-04-30 스프링 빈 조회 - 상속관계 2022-05-01 XML 생성 2022-05-02 웹어플리케이션 과 싱글톤 2022-05-03 싱글톤 컨테이너 2022-05-03 싱글톤 방식의 주의점 2022-05-08 컴포넌트 스캔을 통한 의존관계 자동 주입

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
     // 현재 추상화에도 의존한고 구체화에도 문제가됨 (나중에 변경할때 커다란 문제가 될 수 있음 DIP를 위반!)
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository;
 
+    @Autowired //ac.getBean(MemberRepository.class)와 같은 기능
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     //원래 동시성 이슈가 있어서 컨커러 해쉬맵을 사용 해야됨(실무)
