7cca0dea9dc10880040e7fed4d398e082885fdfa
sagara12 <sagara12@naver.com>
2022-05-10 11:24:51 +0900

    0220-04-13 프로젝트(개인) 커밋 2022-04-14 프로젝트(개인) 기본 기능 구현 2022-04-15 제이유닛 테스트 기본 설정 2022-04-15 주문과 할인 기능 구현 2022-04-15 주문과 할인 기능 제이유닛 테스트 구현 2022-04-16 할인 정책 변경 및 제이유닛 테스트 2022-04-25 의존성 주입형식으로 변경 2022-04-25 DI 컨테이너 생성 2022-04-26 스프링 변환 2022-04-28 컨테이너에 등록된 빈 조회 2022-04-28 스프링 빈 조회 2022-04-28 동일한 타입이 둘 이상일경우 스프링 빈 조회 2022-04-30 스프링 빈 조회 - 상속관계 2022-05-01 XML 생성 2022-05-02 웹어플리케이션 과 싱글톤 2022-05-03 싱글톤 컨테이너 2022-05-03 싱글톤 방식의 주의점 2022-05-08 컴포넌트 스캔을 통한 의존관계 자동 주입 2022-05-08 기본 스캔 대상 지정 2022-05-09 필터 2022-05-09 중복 등록과 충돌 2022-05-09 의존관계 주입 방법 2022-05-10 옵션 처리 및 오류 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -32,8 +32,8 @@ public class AppConfig {
     @Bean
     public OrderService orderService() {
         System.out.println("call AppConfig.orderService");
-//        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
-        return null;
+        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
+//        return null;
     }
 
     @Bean
--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -41,7 +41,6 @@ class ApplicationContextBasicFindTest {
     @Test
     @DisplayName("빈 이름으로 조회 X")
     void findBeanByNameX(){
-        ac.getBean("xxxxx",MemberService.class);
         org.junit.jupiter.api.Assertions.assertThrows(NoSuchBeanDefinitionException.class,
         ()-> ac.getBean("xxxxx",MemberService.class));
     }
