217fe717cdddec297cc149529c0676bbd60c2288
sagara12 <sagara12@naver.com>
2022-05-13 15:18:06 +0900

    0220-04-13 프로젝트(개인) 커밋 2022-04-14 프로젝트(개인) 기본 기능 구현 2022-04-15 제이유닛 테스트 기본 설정 2022-04-15 주문과 할인 기능 구현 2022-04-15 주문과 할인 기능 제이유닛 테스트 구현 2022-04-16 할인 정책 변경 및 제이유닛 테스트 2022-04-25 의존성 주입형식으로 변경 2022-04-25 DI 컨테이너 생성 2022-04-26 스프링 변환 2022-04-28 컨테이너에 등록된 빈 조회 2022-04-28 스프링 빈 조회 2022-04-28 동일한 타입이 둘 이상일경우 스프링 빈 조회 2022-04-30 스프링 빈 조회 - 상속관계 2022-05-01 XML 생성 2022-05-02 웹어플리케이션 과 싱글톤 2022-05-03 싱글톤 컨테이너 2022-05-03 싱글톤 방식의 주의점 2022-05-08 컴포넌트 스캔을 통한 의존관계 자동 주입 2022-05-08 기본 스캔 대상 지정 2022-05-09 필터 2022-05-09 중복 등록과 충돌 2022-05-09 의존관계 주입 방법 2022-05-10 옵션 처리 및 오류 수정 2022-05-10 생성자 주입을 써야 하는 이유 2022-05-10 롬북으로 코드 깔끔하게 만들기 2022-05-11 Qualifier, Primarty 추가 2022-05-11 어노테이션 직접 만들기 2022-05-11 조회한 빈이 모두 필요할때 List, Map으로 받아오기 2022-05-12 빈 생명 주기 콜백 2022-05-12 @PostConstruct, @PreDestroy 추가 2022-05-12 프로토타입 스코프 2022-05-13 프로토타입 스코프와 싱글톤을 같이 사용할때 생기는 문제점 2022-05-13 Provider를 사용한 문제해결

--- src/test/java/hello/core/scope/SingletonWithPrototype.java
+++ src/test/java/hello/core/scope/SingletonWithPrototype.java
@@ -3,12 +3,14 @@ package hello.core.scope;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototype {
 
@@ -35,19 +37,23 @@ public class SingletonWithPrototype {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; //생성시점에 주입
+        //private final PrototypeBean prototypeBean; //생성시점에 주입
 
         @Autowired
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+        /*@Autowired
         public ClientBean(PrototypeBean prototypeBean) {
             this.prototypeBean = prototypeBean;
-        }
+        }*/
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
